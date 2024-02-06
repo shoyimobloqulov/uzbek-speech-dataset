c2c99871a2c59bc6074b0dbc6fd0cd032115abc5
ShoyimObloqulov <obloqulovshoyim1@gmail.com>
2024-02-06 20:22:57 +0500

    core add file backend

---
+++ core/main_generator.py
@@ -0,0 +1,236 @@
+from rich import print
+from rich.console import Console
+import glob
+import os
+from spacy.lang.de import German
+from spacy.lang.en import English
+from spacy.lang.fr import French
+from spacy.lang.it import Italian
+import time
+import pyaudio
+import keyboard
+from rich.progress import Progress
+from rich.progress import track
+from rich.table import Table
+import wave
+from datetime import datetime
+import librosa
+import struct
+import numpy as np
+import webrtcvad
+from scipy.ndimage import binary_dilation
+
+import soundfile
+import sys
+import csv
+
+# Audio processing
+chunk = 1024
+sample_format = pyaudio.paInt16
+channels = 1
+frame_rate = 22050
+timeout = 60
+
+# For silence trimming
+vad_moving_average_width = 8
+int16_max = (2 ** 15) - 1
+vad_window_length = 30
+sample_rate = 16000
+vad_max_silence_length = 6
+
+
+# Source: CorentinJ - Real Time Voice Cloning - Thanks for this one!
+def trim_long_silences(wav):
+    # Compute the voice detection window size
+    samples_per_window = (vad_window_length * sample_rate) // 1000
+
+    # Trim the end of the audio to have a multiple of the window size
+    wav = wav[:len(wav) - (len(wav) % samples_per_window)]
+
+    # Convert the float waveform to 16-bit mono PCM
+    pcm_wave = struct.pack("%dh" % len(wav), *(np.round(wav * int16_max)).astype(np.int16))
+
+    # Perform voice activation detection
+    voice_flags = []
+    vad = webrtcvad.Vad(mode=3)
+    for window_start in range(0, len(wav), samples_per_window):
+        window_end = window_start + samples_per_window
+        voice_flags.append(vad.is_speech(pcm_wave[window_start * 2:window_end * 2], sample_rate=sample_rate))
+    voice_flags = np.array(voice_flags)
+
+    # Smooth the voice detection with a moving average
+    def moving_average(array, width):
+        array_padded = np.concatenate((np.zeros((width - 1) // 2), array, np.zeros(width // 2)))
+        ret = np.cumsum(array_padded, dtype=float)
+        ret[width:] = ret[width:] - ret[:-width]
+        return ret[width - 1:] / width
+
+    audio_mask = moving_average(voice_flags, vad_moving_average_width)
+    audio_mask = np.round(audio_mask).astype(np.bool)
+
+    # Dilate the voiced regions
+    audio_mask = binary_dilation(audio_mask, np.ones(vad_max_silence_length + 1))
+    audio_mask = np.repeat(audio_mask, samples_per_window)
+
+    return wav[audio_mask == True]
+
+
+if __name__ == '__main__':
+
+    console = Console()
+
+    table = Table()
+    table.add_column("TSS ma'lumotlar to'plami yaratuvchisi", style="cyan")
+    table.add_row("2021 - OSH")
+    table.add_row("t.me/shoyim_obloqulov")
+
+    console.print(table)
+
+    console.print("\nPlease select your [red]microphone[/red] (enter the device number).")
+    # Initialisiere pyaudio
+    pyaudio = pyaudio.PyAudio()
+
+    # select input device
+    info = pyaudio.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    for i in range(0, numdevices):
+        if (pyaudio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device id ", i, " - ", pyaudio.get_device_info_by_host_api_device_index(0, i).get('name'))
+    in_mic_id = int(input())
+    console.print(
+        "You have selected [red]%s[/red] as input device." % pyaudio.get_device_info_by_host_api_device_index(0,
+                                                                                                              in_mic_id).get(
+            'name'))
+
+    # Select a project folder
+    app_folder = os.path.dirname(os.path.realpath(__file__))
+    project_directories = glob.glob(os.path.join(app_folder, 'project*/'))
+
+    if len(project_directories) == 0:
+        console.print(
+            "There are no project directories. Create a folder starting with 'project' that contains a 'metadata.csv'. This works best using main_generate_csv.py.")
+        sys.exit(0)
+
+    console.print(
+        "Please select a [red]project folder[/red] by entering its id (default 0 (%s))" % project_directories[0])
+    for index, pd in enumerate(project_directories):
+        console.print("%d: %s" % (index, pd))
+
+    in_folder_id = input()
+    if not in_folder_id:
+        in_folder_id = 0
+    project_folder = project_directories[int(in_folder_id)]
+
+    metadata_csv_file = os.path.join(project_folder, 'metadata.csv')
+    if not os.path.exists(metadata_csv_file):
+        console.print("Project folder does not contain a metadata.csv file. Exiting.")
+        sys.exit(0)
+
+    console.print("wavs will be saved in [red]%s" % project_folder)
+
+    # 0.1 select languages
+    console.print("Please select a [red]language[/red] (default [i]%s[/i])." % 'de')
+    in_lang = input()
+    if not in_lang:
+        in_lang = 'de'
+    console.print("Language is set to [red]%s[/red]." % in_lang)
+
+    # load csv
+    with open(metadata_csv_file, encoding="utf-8") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter='|')
+        csv_data = list(csv_reader)
+
+    console.print("Found %d sentences." % len(csv_data))
+
+    if len(csv_data) == 0:
+        console.print("You need to add some sentences to your text files first. Exiting.")
+        sys.exit(0)
+
+    # 1.1 Hit Space to skip to save+next, hit backspace to discard
+    console.print(
+        "[green]n[/green] = next sentence, [yellow]d[/yellow] = discard and repeat last recording, [red]e[/red] = exit recording.")
+
+    console.print("Ready?", style="green")
+    input("Press Enter to start")
+
+    i = 0
+    cancelled = False
+    while i < len(csv_data):
+        console.clear()
+
+        current_sentence = csv_data[i][1]
+        current_sentence = current_sentence.replace("\n", " ")
+        current_sentence = current_sentence.replace("\t", " ")
+
+        # If this wav file has been recorded before, continue
+        wav_file_name = csv_data[i][0]
+        if os.path.exists(os.path.join(project_folder, wav_file_name)):
+            i += 1
+            continue
+
+        console.print("\n\n" + current_sentence + "\n\n", style="black on white", justify="center")
+        console.print(
+            "(%d/%d) [green]n[/green] = next sentence, [yellow]d[/yellow] = discard and repeat last recording, [blue]s[/blue] = skip, [red]e[/red] = exit recording." % (
+            (i + 1), len(csv_data)))
+
+        start_time = time.time()
+        current_time = time.time()
+        frames = []
+        stream = pyaudio.open(input_device_index=in_mic_id, format=sample_format, channels=channels, rate=frame_rate,
+                              frames_per_buffer=chunk, input=True)
+
+        with Progress() as recording_progress:
+            recording_task = recording_progress.add_task("[red]Recording...", total=timeout)
+
+            # while (current_time - start_time) < timeout:
+            while not recording_progress.finished:
+                recording_progress.update(recording_task, completed=current_time - start_time)
+
+                data = stream.read(chunk)
+                frames.append(data)
+                current_time = time.time()
+
+                if keyboard.is_pressed('n'):
+                    while keyboard.is_pressed('n'):
+                        time.sleep(0.1)
+
+                    # Write the wav file
+                    data = stream.read(chunk)
+                    frames.append(data)
+                    stream.close()
+                    wf = wave.open(os.path.join(project_folder, wav_file_name), 'wb')
+                    wf.setnchannels(channels)
+                    wf.setsampwidth(pyaudio.get_sample_size(sample_format))
+                    wf.setframerate(frame_rate)
+                    wf.writeframes(b''.join(frames))
+                    wf.close()
+
+                    # trim silence?
+                    wav, source_sr = librosa.load(str(os.path.join(project_folder, wav_file_name)), sr=None)
+                    wav = trim_long_silences(wav)
+                    soundfile.write(str(os.path.join(project_folder, wav_file_name)), wav, source_sr)
+
+                    i += 1
+                    break
+                elif keyboard.is_pressed("s"):
+                    while keyboard.is_pressed('s'):
+                        time.sleep(0.1)
+                    i += 1
+                    stream.close()
+                    break
+                elif keyboard.is_pressed("d"):
+                    while keyboard.is_pressed('d'):
+                        time.sleep(0.1)
+                    stream.close()
+                    break
+                elif keyboard.is_pressed("e"):
+                    while keyboard.is_pressed('e'):
+                        time.sleep(0.1)
+                    stream.close()
+                    cancelled = True
+                    break
+
+            if cancelled:
+                break
+
+    pyaudio.terminate()
---
+++ core/main_text_gen_gpt2.py
@@ -0,0 +1,200 @@
+import decimal
+from rich import print
+from rich.console import Console
+from rich.table import Table
+from rich.progress import track
+import wikipedia
+import re
+import os
+import random
+import sys
+from num2words import num2words
+
+from spacy.lang.de import German
+from spacy.lang.en import English
+from spacy.lang.fr import French
+from spacy.lang.it import Italian
+
+from transformers import pipeline
+
+currencies = {
+    'de_$': 'Dollar',
+    'de_€': 'Euro',
+    'de_₿': 'Bitcoin',
+    'de_£': 'Pfund',
+    'en_$': 'dollar',
+    'en_€': 'euro',
+    'en_₿': 'bitcoin',
+    'en_£': 'pound',
+}
+
+
+def normlize_text(text, lang, nlp):
+    # tokenize
+    doc = nlp(text)
+    res = []
+    for token in doc:
+        new_token = ''
+
+        # numbers
+        if token.like_num:
+            try:
+                # ordinals
+                if token.text.endswith('.'):
+                    # new_token = num2words(token.text, lang=lang, to = 'ordinal')
+                    new_token = token
+                else:
+                    new_token = num2words(token.text, lang=lang)
+            except NotImplementedError:
+                print("Warning, language %s not supported for text normalization." % lang)
+                new_token = token.text
+        # currencies
+        elif token.is_currency:
+            if lang + '_' + token.text in currencies:
+                new_token = currencies[lang + '_' + token.text]
+            else:
+                new_token = token.text
+        else:
+            new_token = token.text
+        res.append(new_token)
+    sentence = ' '.join(res)
+
+    return sentence
+
+
+if __name__ == '__main__':
+
+    console = Console()
+
+    app_folder = os.path.dirname(os.path.realpath(__file__))
+    texts_folder = os.path.join(app_folder, "texts")
+
+    table = Table()
+    table.add_column("TSS GPT Text Generator", style="cyan")
+    table.add_row("2021 - padmalcom")
+    table.add_row("www.stonedrum.de")
+    table.add_row("This tool generates texts using GPT-2 language models.")
+
+    # Select language
+    default_lang = 'de'
+    console.print("Please select a [red]language[/red] to collect data for (default [i]%s[/i])." % default_lang)
+    in_lang = input()
+    if not in_lang:
+        in_lang = default_lang
+
+    if not in_lang in ['de', 'en', 'fr', 'it']:
+        console.print("Language [red]%s[/red] is not supported for gpt2 generation." % in_lang)
+        sys.exit(0)
+
+    console.print("Language is set to [red]%s[/red]." % in_lang)
+
+    # Select amount of files
+    default_file_count = 100
+    console.print("Please select a [red]file count[/red] (default [i]%d[/i])." % default_file_count)
+    in_file_count = input()
+    if not in_file_count or not in_file_count.isdigit():
+        in_file_count = default_file_count
+    in_file_count = int(in_file_count)
+    console.print("Will collect [red]%d[/red] files." % in_file_count)
+
+    # Normalize and clean text
+    console.print("Please specify if you want to [red]normalize (n)[/red] texts. (default [i]none[/i]).")
+    normalize_and_clean = input()
+    normalize_text = 'n' in normalize_and_clean
+    console.print("Normlize text? [red]%r[/red]." % normalize_text)
+
+    # split texts by sentences
+    if in_lang == 'de':
+        nlp = German()
+        # pipe = pipeline('text-generation', model="benjamin/gerpt2-large", tokenizer="benjamin/gerpt2-large", device = 0)
+        pipe = pipeline('text-generation', model='Tanhim/gpt2-model-de', tokenizer='Tanhim/gpt2-model-de')
+        text = pipe("Der Sinn des Lebens ist es", max_length=100)[0]["generated_text"]
+    elif in_lang == 'en':
+        nlp = English()
+        pipe = pipeline('text-generation', model="gpt2", device=0)
+        text = pipe("The meaning of life is", max_length=100)[0]["generated_text"]
+    elif in_lang == 'fr':
+        nlp = French()
+        pipe = pipeline('text-generation', model="antoiloui/belgpt2", tokenizer="antoiloui/belgpt2", device=0)
+        text = pipe("Le sens de la vie est", max_length=100)[0]["generated_text"]
+    elif in_lang == 'it':
+        nlp = Italian()
+        pipe = pipeline('text-generation', model="LorenzoDeMattei/GePpeTto", tokenizer="LorenzoDeMattei/GePpeTto",
+                        device=0)
+        text = pipe("Il senso della vita è", max_length=100)[0]["generated_text"]
+    else:
+        console.print("The language %s is not supported yet. Please create a github issue." % in_lang)
+        sys.exit(0)
+
+    nlp.add_pipe('sentencizer')
+
+    # Get all sentences from text except the first one
+    doc = nlp(text)
+
+    # Keep sentences with a proper ending
+    all_sentences = [str(sent).strip() for sent in doc.sents if str(sent).strip().endswith(('.', '!', '?'))]
+
+    # If there are enough sentences, remove the one used for generation
+    if len(all_sentences) > 1:
+        all_sentences = all_sentences[1:]
+
+    gen_sentence = ' '.join(all_sentences)
+    console.print("Initial text: [magenta]%s" % gen_sentence)
+
+    # Generate as much text as desired
+    files_written = 0
+    # for n in track(range(in_file_count), description="Processing..."):
+    n = 0
+    while n < in_file_count:
+
+        console.print("(%d/%d) Processing sentence ..." % ((n + 1), in_file_count))
+
+        # Generate new sentences based on the last n characters
+        prefix = gen_sentence[-min(300, len(gen_sentence)):]
+        # console.print("Using prefix [green]%s[/green] of text [green]%s[/green] for generation." % (prefix, gen_sentence))
+        gen_result = pipe(prefix, max_length=150)
+        if len(gen_result) > 0:
+            text = pipe(prefix, max_length=150)[0]["generated_text"]
+        else:
+            continue
+
+        if len(text) == 0:
+            print("No valid output was generated. Please restart.")
+            sys.exit(0)
+
+        # cut the prefix
+        text = text[len(prefix):]
+        doc = nlp(text)
+        all_new_sentences = [str(sent).strip() for sent in doc.sents if str(sent).strip().endswith(('.', '!', '?'))]
+
+        # If there are enough sentences, remove the one used for generation
+        if len(all_new_sentences) > 1:
+            all_new_sentences = all_new_sentences[1:]
+
+        gen_sentence = ' '.join(all_new_sentences)
+        console.print("Generated sentence(s): %s" % gen_sentence)
+
+        if normalize_text:
+            gen_sentence = normlize_text(gen_sentence, in_lang, nlp)
+
+        # remove brackets
+        gen_sentence = re.sub(".*?\((.*?)\).*?", "", gen_sentence)
+        gen_sentence = re.sub(".*?\[(.*?)\].*?", '', gen_sentence)
+
+        # replace linebreaks and tabs
+        gen_sentence = gen_sentence.replace("\n", " ")
+        gen_sentence = gen_sentence.replace("\t", " ")
+
+        i = 0
+        while os.path.exists(os.path.join(texts_folder, in_lang + "_gpt" + str(i) + ".txt")):
+            i += 1
+
+        text_file_path = os.path.join(texts_folder, in_lang + "_gpt" + str(i) + ".txt")
+        text_file = open(text_file_path, 'a', encoding='utf-8')
+        text_file.write(gen_sentence)
+        text_file.close()
+        files_written += 1
+        n += 1
+
+    console.print("%d files were written." % files_written)
+
